49623dececd5b107272e35236b1e116337c352a1
João Júnior <joaojunior.b22@gmail.com>
2022-08-02 16:46:21 -0300

    pre_fix

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const app = express()
 const port = process.env.port || 8080
+const app = express()
 
 // create the connection to database
 const mysql = async ( f, data )=>{
