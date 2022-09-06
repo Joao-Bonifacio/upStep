5bd206b7161c0aa2a40a1ed25c77cd02fbcb57ea
João Bonifácio <joaojunior.b22@gmail.com>
2022-09-06 15:27:29 -0300

    react-worcking

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,13 +1,15 @@
+import React from 'react'
+import { Button } from 'reactstrap'
 import ApiData from './components/apiClient'
 import './App.css'
 
 function App() {
   //document.cookie = 'key=04ff27090f4978d7f32636422abfb4e9; expires=Thu, 29 Nov 00:00:00 UTC;'
-
   return (
     <>
       <main>
         <ApiData/>
+        <Button color="danger">Danger!</Button>
       </main>
     </>
     )
