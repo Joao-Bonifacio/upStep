3a3cb5177e4b30c591a8be5dec5be7e7b5821834
João Bonifácio <joaojunior.b22@gmail.com>
2022-09-09 13:59:50 -0300

    signup-finsished

--- client/app/src/components/Signup.js
+++ client/app/src/components/Signup.js
@@ -29,41 +29,48 @@ export default function Signup(){
                         </div>
 
                         <div className="col-6">
-                            <div className="form-check">
+                            <p className="form-check">
                                 <input className="form-check-input" type="radio" name="sex" id="M" value="M" defaultChecked/>
                                 <label className="form-check-label" htmlfor="M">Are you Male?</label>
-                            </div>
+                            </p>
                             <br/>
-                            <div className="form-check">
+                            <p className="form-check">
                                 <input className="form-check-input" type="radio" name="sex" id="F" value="F"/>
                                 <label className="form-check-label" htmlfor="F">Are you Female?</label>
-                            </div>
+                            </p>
                         </div>
 
                     </div>
 
-                    <div className="mb-3">
-                        <label forhtml="password" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" name="password" required/>
-                    </div>
-                    <div className="mb-3">
-                        <label forhtml="cpassword" className="form-label">Confirm Password</label>
-                        <input type="password" className="form-control" id="cpassword" name="cpassword" required/>
+                    <div className="row">
+                        <div className="mb-3 col-6">
+                            <label forhtml="password" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="password" name="password" required/>
+                        </div>
+                        <div className="mb-3 col-6">
+                            <label forhtml="cpassword" className="form-label">Confirm Password</label>
+                            <input type="password" className="form-control" id="cpassword" name="cpassword" required/>
+                        </div>
                     </div>
 
-                    <select className="form-select" aria-label="Default select" name="country">
-                        <option selected>Select your country</option>
-                        <option value="Brasil">Brasil</option>
-                        <option value="United States">United States</option>
-                        <option value="Canada">Canada</option>
-                        <option value="Japan">Japan</option>
-                        <option value="England">England</option>
-                    </select>   
-
-                    <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="check" required/>
-                        <label className="form-check-label" forhtml="check">I promise that i will studdy a lot</label>
+                    <div className="row">
+                        <div className="col-6">
+                            <select className="form-select" aria-label="Default select" name="country">
+                                <option selected>Select your country</option>
+                                <option value="Brasil">Brasil</option>
+                                <option value="United States">United States</option>
+                                <option value="Canada">Canada</option>
+                                <option value="Japan">Japan</option>
+                                <option value="England">England</option>
+                            </select>
+                        </div>   
+    
+                        <div className="mb-3 form-check col-6">
+                            <input type="checkbox" className="form-check-input" id="check" required/>
+                            <label className="form-check-label" forhtml="check">I promise that i will studdy a lot</label>
+                        </div>
                     </div>
+
                     <div className="text-center">
                         <button type="submit" className="btn btn-primary me-3">Signup</button>
                         <span className="ms-3"><a href="http://jj.me:3000/login">Login</a></span>
