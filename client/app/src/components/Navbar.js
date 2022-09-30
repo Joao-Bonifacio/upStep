105eaf4f5c8d6bb9015c6b5386a6dcd19c6a02f4
João Bonifácio <joaojunior.b22@gmail.com>
2022-09-30 16:46:26 -0300

    semi-finished

--- client/app/src/components/Navbar.js
+++ client/app/src/components/Navbar.js
@@ -1,4 +1,11 @@
 export default function Navbar(){
+    const logOut = ()=>{
+        let date = new Date()
+        date.setTime(date.getTime()+(720/60/60/10000))
+        let exp = date.toUTCString()
+        document.cookie = `key=${document.cookie}; expires=${exp};`
+        window.location.reload()
+    }
     return(
         <nav className="navbar navbar-expand-lg bg-primary">
             <div className="container-fluid">
@@ -12,19 +19,17 @@ export default function Navbar(){
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">Features</a>
+                            <span className="nav-link breve" onClick={(e)=>{e.preventDefault();alert('coming soon...')}}>Forum</span>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">Pricing</a>
+                            <a className="nav-link" href="https://joao-bonifacio.github.io/Portifolio/" target="blank">Author</a>
                         </li>
                         <li className="nav-item dropdown ml-10">
                             <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <i class="fa-regular fa-user"></i>User
                             </a>
                             <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="/">Config</a></li>
-                                <li><a className="dropdown-item" href="/">Another action</a></li>
-                                <li><a className="dropdown-item" href="/">Something else here</a></li>
+                                <li><button className="dropdown-item" onClick={logOut}>Log Out</button></li>
                             </ul>
                         </li>
                     </ul>
